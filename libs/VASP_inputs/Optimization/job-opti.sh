f2ffded426259badf0b805bc6e2cf43149de60ab
Hanpu_Liang <vento_l@hotmail.com>
2021-12-21 19:12:29 +0800

    update optimization in post process

--- libs/VASP_inputs/Optimization/job-opti.sh
+++ libs/VASP_inputs/Optimization/job-opti.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 cp POSCAR POSCAR_0
+DPT -v potcar
 for i in {1..3}
 do
     cp INCAR_$i INCAR
     cp KPOINTS_$i KPOINTS
-    mpirun -machinefile $PBS_NODEFILE -np $NP vasp_std > vasp.out
+    date > vasp-$i.vasp
+    mpirun -machinefile $PBS_NODEFILE -np $NP vasp_std >> vasp-$i.vasp
+    date >> vasp-$i.vasp
     cp CONTCAR POSCAR
     cp CONTCAR POSCAR_$i
     cp OUTCAR OUTCAR_$i
